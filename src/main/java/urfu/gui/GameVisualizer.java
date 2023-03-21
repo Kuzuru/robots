db801dde1a9b9d8c6be97b43b708cbab95bc3b24
Artem Kiselev <kuzuru.dev@gmail.com>
2023-03-21 11:44:05 +0500

    refactor: Разделена логика MainApplicationFrame

--- src/main/java/urfu/gui/GameVisualizer.java
+++ src/main/java/urfu/gui/GameVisualizer.java
@@ -117,42 +117,53 @@ public class GameVisualizer extends JPanel
 
     protected void onModelUpdateEvent()
     {
-        double distance = distance(m_targetPositionX, m_targetPositionY,
-                m_robotPositionX, m_robotPositionY);
+        double distance = distance(
+                m_targetPositionX, m_targetPositionY,
+                m_robotPositionX, m_robotPositionY
+        );
+
         if (distance < 0.5) {
             return;
         }
-        double velocity = maxVelocity;
+
         double angleToTarget = angleTo(m_robotPositionX, m_robotPositionY, m_targetPositionX, m_targetPositionY);
         double angularVelocity = 0;
+
         if (angleToTarget > m_robotDirection) {
             angularVelocity = maxAngularVelocity;
         }
+
         if (angleToTarget < m_robotDirection) {
             angularVelocity = -maxAngularVelocity;
         }
 
-        moveRobot(velocity, angularVelocity, 10);
+        moveRobot(maxVelocity, angularVelocity, 10);
     }
 
     private void moveRobot(double velocity, double angularVelocity, double duration)
     {
         velocity = applyLimits(velocity, 0, maxVelocity);
         angularVelocity = applyLimits(angularVelocity, -maxAngularVelocity, maxAngularVelocity);
+
         double newX = m_robotPositionX + velocity / angularVelocity *
                 (Math.sin(m_robotDirection + angularVelocity * duration) -
                         Math.sin(m_robotDirection));
+
         if (!Double.isFinite(newX)) {
             newX = m_robotPositionX + velocity * duration * Math.cos(m_robotDirection);
         }
+
         double newY = m_robotPositionY - velocity / angularVelocity *
                 (Math.cos(m_robotDirection + angularVelocity * duration) -
                         Math.cos(m_robotDirection));
+
         if (!Double.isFinite(newY)) {
             newY = m_robotPositionY + velocity * duration * Math.sin(m_robotDirection);
         }
+
         m_robotPositionX = newX;
         m_robotPositionY = newY;
+
         double newDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration);
         m_robotDirection = newDirection;
     }
