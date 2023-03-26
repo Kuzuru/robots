8bbc7c58cc1532ba539ab54f65ab88370c8f5b8b
Artem Kiselev <kuzuru.dev@gmail.com>
2023-03-26 23:53:26 +0500

    feat: Восстановил Drawer

---
+++ src/main/java/urfu/model/Drawer.java
@@ -0,0 +1,16 @@
+package urfu.model;
+
+import java.awt.*;
+
+public class Drawer
+{
+    protected static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
+    {
+        g.fillOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    }
+
+    protected static void drawOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
+    {
+        g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    }
+}
